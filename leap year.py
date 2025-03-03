781b185c43c16c37292593655179f69ddfaa319f
sandiezan <sandie.zanolin@gmail.com>
2025-03-03 11:04:33 +0000

    Delete leap year.py

--- leap year.py
+++
@@ -1,9 +0,0 @@
-#This exercise aims to determine whether the year inputted by the user is a leap year or not
-year = input("Please select a year: ")
-
-year = int(year)
-
-if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-    print("This is a leap year")
-else:
-    print("This is a non leap year")
